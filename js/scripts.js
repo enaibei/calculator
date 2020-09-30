b2c00054e2a54683316ac6852d357aab760d8e5d
Esther.Naibei <esthernaibei2020@gmail.com>
2020-09-30 14:03:52 +0300

    add initial files - index.html. js, css,readme.md

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2){
+    return number1+number2;
+    }
+    var number1 = prompt("Enter first number");
+    var number2 = prompt("Enter second number");
+    alert(add(number1, number2));
