d468b7aa5ba52c4ed64d80be320411078a2d6b38
Esther.Naibei <esthernaibei2020@gmail.com>
2020-09-30 17:40:49 +0300

    add cooking.js for converting gallons to kgs

---
+++ js/cooking.js
@@ -0,0 +1,6 @@
+var gallonsToLitres = function(gallonUnit){
+    return UnitsInKilograms;
+}
+var gallonUnit = parseFloat(prompt("Enter units in gallons "));
+UnitsInKilograms = gallonUnit*3.78541;
+alert(UnitsInKilograms)
